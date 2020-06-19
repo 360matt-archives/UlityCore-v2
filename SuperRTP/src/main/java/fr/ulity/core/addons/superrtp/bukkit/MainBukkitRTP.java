363e362d1c24b8fe7f62db802181181ced60a737
360matt <mattbacq2004@gmail.com>
2020-06-19 11:39:14 +0200

    Ajouts de traductions anglaises et ajout de la télémétrie

--- SuperRTP/src/main/java/fr/ulity/core/addons/superrtp/bukkit/MainBukkitRTP.java
+++ SuperRTP/src/main/java/fr/ulity/core/addons/superrtp/bukkit/MainBukkitRTP.java
@@ -5,11 +5,12 @@ import fr.ulity.core.addons.superrtp.bukkit.events.InventoryEventRTP;
 import fr.ulity.core.api.Api;
 import fr.ulity.core.api.Config;
 import fr.ulity.core.api.Initializer;
+import fr.ulity.core.api.Metrics;
 import net.milkbowl.vault.economy.Economy;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.bukkit.Bukkit.getPluginManager;
@@ -50,6 +51,10 @@ public final class MainBukkitRTP extends JavaPlugin {
         Initializer init = new Initializer(this);
         init.requireVersion("2.1.1");
         init.reloadLang();
+
+        Metrics metrics = new Metrics(this, 7891);
+        metrics.addCustomChart(new Metrics.SimplePie("others_plugins", () -> Arrays.toString(getServer().getPluginManager().getPlugins())));
+
 
         if (init.ok) {
             config = new Config("config", "super_rtp");

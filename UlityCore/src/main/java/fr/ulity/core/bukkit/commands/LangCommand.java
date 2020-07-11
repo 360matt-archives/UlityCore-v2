34d2f12a6929660d4b7365ea2bae1bb9620f09dd
360matt <mattbacq2004@gmail.com>
2020-07-11 23:44:06 +0200

    Implémentation des nouveautés du module linguistique

--- UlityCore/src/main/java/fr/ulity/core/bukkit/commands/LangCommand.java
+++ UlityCore/src/main/java/fr/ulity/core/bukkit/commands/LangCommand.java
@@ -8,33 +8,25 @@ import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
-public class LangCommand extends CommandManager {
+public class LangCommand extends CommandManager.Assisted {
     public LangCommand(CommandMap commandMap, JavaPlugin plugin) {
         super(plugin, "lang");
-        addDescription(Lang.get("commands.lang.description"));
-        addUsage(Lang.get("commands.lang.usage"));
+        addDescription(Lang.get("plugin.commands.lang.description"));
+        addUsage(Lang.get("plugin.commands.lang.usage"));
         setAliases(Arrays.asList(Lang.getStringArray("plugin.commands.lang.aliases")));
 
-        addOneTabbComplete(-1, null, "lang");
         registerCommand(commandMap);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+    public void exec(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 1)
-            sender.sendMessage(Lang.get(sender, "plugin.commands.lang.lang_required"));
+            Lang.prepare("plugin.commands.lang.lang_required").sendPlayer(sender);
         else {
             Api.data.set("player." + sender.getName() + ".lang", args[0]);
-            sender.sendMessage(Lang.get(sender, "plugin.commands.lang.lang_changed"));
+            Lang.prepare("plugin.commands.lang.lang_changed").sendPlayer(sender);
         }
-
-
-        return true;
     }
-
 }

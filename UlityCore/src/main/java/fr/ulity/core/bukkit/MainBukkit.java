131c2030393ce3abda2bceb1925cebc06806d092
360matt <mattbacq2004@gmail.com>
2020-07-11 21:49:03 +0200

    Supression de la variable commandMap déjà fournie dans Ai.Bukki.commandMap

--- UlityCore/src/main/java/fr/ulity/core/bukkit/MainBukkit.java
+++ UlityCore/src/main/java/fr/ulity/core/bukkit/MainBukkit.java
@@ -9,7 +9,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 
 public final class MainBukkit extends JavaPlugin {
@@ -41,22 +40,11 @@ public final class MainBukkit extends JavaPlugin {
                         "§e>_   §6" + Initializer.lesPlugins.size() + " §bextensions loaded");
             });
 
-            try {
-                Field f = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-                f.setAccessible(true);
-                commandMap = (CommandMap) f.get(Bukkit.getServer());
 
-                new UlityCoreCommand(commandMap, this);
-                new LangCommand(commandMap, this);
+            new UlityCoreCommand(Api.Bukkit.commandMap, this);
+            new LangCommand(Api.Bukkit.commandMap, this);
 
-
-                // getServer().getPluginManager().registerEvents(new X(), this);
-
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                getLogger().severe(e.getLocalizedMessage());
-                e.printStackTrace();
-                this.getPluginLoader().disablePlugin(this);
-            }
+            // getServer().getPluginManager().registerEvents(new X(), this);
 
         }
     }

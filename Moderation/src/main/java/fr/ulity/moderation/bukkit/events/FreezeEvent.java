3e12ce9a5f2a24df6a8dc57e4040870c83b502bb
360matt <mattbacq2004@gmail.com>
2020-07-11 21:34:38 +0200

    Fix bug clic droit pour bouffer & arc & enderpearl à cause de l'Event Interact défectueux (deprecated), maintenant fixés.

--- Moderation/src/main/java/fr/ulity/moderation/bukkit/events/FreezeEvent.java
+++ Moderation/src/main/java/fr/ulity/moderation/bukkit/events/FreezeEvent.java
@@ -13,65 +13,59 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-
 public class FreezeEvent implements Listener {
 
-    private static boolean isMod (Player player) {
-        return player.hasPermission("ulity.mod") || player.hasPermission("ulity.mod.freeze");
+    private static boolean isNotMod (Player player) {
+        return !player.hasPermission("ulity.mod") && !player.hasPermission("ulity.mod.freeze");
     }
 
     @EventHandler
     private static void onJoin (PlayerJoinEvent e) {
         Freeze playerFreeze = new Freeze(e.getPlayer().getName());
-
-        if (playerFreeze.isFreeze() && !isMod(e.getPlayer()))
-            playerFreeze.recallFreeze();
-        else
-            playerFreeze.recallUnFreeze();
+        if (playerFreeze.isFreeze() && isNotMod(e.getPlayer())) playerFreeze.recallFreeze();
+        else playerFreeze.recallUnFreeze();
     }
 
     @EventHandler
     private static void onChat (AsyncPlayerChatEvent e) {
-        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && !isMod(e.getPlayer())));
+        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && isNotMod(e.getPlayer())));
     }
 
     @EventHandler
     private static void onPvP (EntityDamageByEntityEvent e) {
         if (e.getEntity() instanceof Player) {
-            if (new Freeze(e.getEntity().getName()).isFreeze() && !isMod((Player) e.getEntity()))
+            if (new Freeze(e.getEntity().getName()).isFreeze() && isNotMod((Player) e.getEntity()))
                 e.setCancelled(true);
         }
         else if (e.getDamager() instanceof Player)
-            if (new Freeze(e.getDamager().getName()).isFreeze() && !isMod((Player) e.getDamager()))
+            if (new Freeze(e.getDamager().getName()).isFreeze() && isNotMod((Player) e.getDamager()))
                 e.setCancelled(true);
     }
 
     @EventHandler
     private static void onPlace (BlockPlaceEvent e) {
-        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && !isMod(e.getPlayer())));
+        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && isNotMod(e.getPlayer())));
     }
 
     @EventHandler
-    private static void onCommand (BlockBreakEvent e) {
-        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && !isMod(e.getPlayer())));
+    private static void onBreak (BlockBreakEvent e) {
+        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && isNotMod(e.getPlayer())));
     }
 
-    @EventHandler
-    @SuppressWarnings("deprecation")
-    private static void onCommand (PlayerInteractEvent e) {
-        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && !isMod(e.getPlayer())));
+     @EventHandler
+    private static void onInteract (PlayerInteractEvent e) {
+        if (new Freeze(e.getPlayer().getName()).isFreeze() && isNotMod(e.getPlayer()))
+            e.setCancelled(true);
     }
 
     @EventHandler
     private static void onCommand (PlayerCommandPreprocessEvent e) {
-        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && !isMod(e.getPlayer())));
+        e.setCancelled(e.isCancelled() || (new Freeze(e.getPlayer().getName()).isFreeze() && isNotMod(e.getPlayer())));
     }
 
     @EventHandler
-    private static void onCommand (EntityDamageEvent e) {
+    private static void onDamage (EntityDamageEvent e) {
         if (e.getEntity() instanceof Player)
-            e.setCancelled(e.isCancelled() || (new Freeze(e.getEntity().getName()).isFreeze() && !isMod((Player) e.getEntity())));
+            e.setCancelled(e.isCancelled() || (new Freeze(e.getEntity().getName()).isFreeze() && isNotMod((Player) e.getEntity())));
     }
-
-
 }

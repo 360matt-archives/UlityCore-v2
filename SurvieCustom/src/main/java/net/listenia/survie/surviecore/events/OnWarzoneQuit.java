e6718fdff858b9b496bb76ac9116fcc5b1fb77d3
360matt <mattbacq2004@gmail.com>
2020-07-18 17:57:03 +0200

    Dernier push contenant le plugin SurvieCore, plugin à cadre privée

--- SurvieCustom/src/main/java/net/listenia/survie/surviecore/events/OnWarzoneQuit.java
+++ SurvieCustom/src/main/java/net/listenia/survie/surviecore/events/OnWarzoneQuit.java
@@ -12,9 +12,9 @@ public class OnWarzoneQuit implements Listener {
 
     private static boolean isInSpawn (Location loc) {
         return loc.getX() >= SurvieCore.config.getOrSetDefault("spawn.min.x", -434)
-                && loc.getX() <= SurvieCore.config.getOrSetDefault("spawn.max.x", -300)
-                && loc.getZ() >= SurvieCore.config.getOrSetDefault("spawn.min.z", 143)
-                && loc.getZ() <= SurvieCore.config.getOrSetDefault("spawn.max.z", 280);
+                && loc.getX() <= SurvieCore.config.getOrSetDefault("spawn.max.x", -299)
+                && loc.getZ() >= SurvieCore.config.getOrSetDefault("spawn.min.z", 142)
+                && loc.getZ() <= SurvieCore.config.getOrSetDefault("spawn.max.z", 279);
     }
 
 
@@ -24,22 +24,30 @@ public class OnWarzoneQuit implements Listener {
         Location loc2 = e.getTo();
 
         if ( ( loc1.getX() != loc2.getX() || loc1.getZ() != loc2.getZ() ) && loc1.getWorld().equals(loc2.getWorld())) {
-
             if(!isInSpawn(loc1) && isInSpawn(loc2)) {
-                if (!e.getPlayer().hasPermission("grade.mod") && !e.getPlayer().hasPermission("mod")) {
+                for (int i = 0; i <= 8; i++)
+                    if (!loc2.clone().subtract(0, i, 0).getBlock().getType().isAir())
+                        return;
 
-                    e.setCancelled(true);
+            if (!e.getPlayer().hasPermission("grade.mod") && !e.getPlayer().hasPermission("mod")) {
 
-                    double deltaX = loc1.getX() - loc2.getX();//Get X Delta
-                    double deltaY = loc1.getY() - loc2.getY();//Get Z delta
-                    double deltaZ = loc1.getZ() - loc2.getZ();//Get Z delta
+                e.setCancelled(true);
 
-                    Vector vec = new Vector(deltaX, deltaY, deltaZ);//Create new vector
-                    vec.normalize();//Normalize it so we don't shoot the player into oblivion
-                    e.getPlayer().setVelocity(vec.multiply(0.3 / (Math.sqrt(Math.pow(deltaX, 2.0) + Math.pow(deltaZ, 2.0)))));
+                double deltaX = loc1.getX() - loc2.getX();//Get X Delta
+                double deltaY = loc1.getY() - loc2.getY();//Get Z delta
+                double deltaZ = loc1.getZ() - loc2.getZ();//Get Z delta
 
-                    e.getPlayer().sendMessage("§7[§cX§7] Tu ne peux pas retourner au spawn");
-                }
+                Vector vec = new Vector(deltaX, deltaY, deltaZ);
+                vec.normalize();
+                e.getPlayer().setVelocity(vec.multiply(0.005 / (Math.sqrt(Math.pow(deltaX, 2.0) + Math.pow(deltaZ, 2.0)))));
+                e.getPlayer().sendMessage("§7[§cX§7] Tu ne peux pas sauter en venant de la warzone");
+            }
+
+
+
+
+
+
             }
         }
     }

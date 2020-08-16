a219c4a335975bb9ff4133cf4246aa242b257b55
360matt <mattbacq2004@gmail.com>
2020-08-16 16:05:45 +0200

    Added Compatibility for BungeeCord Reformat

--- UlityCore/src/main/java/fr/ulity/core/api/Api.java
+++ UlityCore/src/main/java/fr/ulity/core/api/Api.java
@@ -1,108 +1,72 @@
 package fr.ulity.core.api;
 
+import fr.ulity.core.api.bukkit.LangBukkit;
+import fr.ulity.core.api.bungee.LangBungee;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Field;
-import java.net.URISyntaxException;
 
 public class Api {
-    public static boolean ok = true;
-    public static String type;
+    public static PluginType type;
     public static String version;
+    public static String corePath;
+
+    public static boolean ok = true;
 
     public static Config config;
     public static Temp temp;
     public static Data data;
 
-    public static String prefix;
-    public static String full_prefix;
-
-    public static void initialize(@NotNull Object plugin) {
-        File folder = null;
-
-        try {
-            Class.forName("org.bukkit.plugin.Plugin");
-            type = "bukkit";
-
-            Bukkit.define();
-            Bukkit.plugin = (org.bukkit.plugin.Plugin) plugin;
-            version = Bukkit.plugin.getDescription().getVersion();
-            folder = Bukkit.plugin.getDataFolder();
-
-            /* try {
-                Class.forName("com.destroystokyo.paper.VersionHistoryManager$VersionData");
-            }
-            catch (Exception ignored) {
-                Bukkit.plugin.getLogger().severe("UlityCore require a Paper server. ( no Bukkit or Spigot )\n" +
-                        "Because, Paper is 100000% more efficient than Spigot.");
-                Bukkit.plugin.getPluginLoader().disablePlugin(Bukkit.plugin);
-                ok = false;
-            } */
-        }
-        catch (Exception ignored) {
-            try {
-                Class.forName("net.md_5.bungee.api.plugin.Plugin");
-                type = "bungeecord";
-
-                Bungee.define();
-                Bungee.plugin = (net.md_5.bungee.api.plugin.Plugin) plugin;
-                version = Bungee.plugin.getDescription().getVersion();
-                folder = Bungee.plugin.getDataFolder();
-            }
-            catch (Exception ignored2) {
-                System.out.println("§cWTF ! Bukkit/Spigot/Paper or BungeeCord/Waterfall are not detected. WtttFF");
-                ok = false;
-            }
-        }
-
-        if (ok){
-            full_prefix = folder.getAbsolutePath();
-            prefix = folder.toString().replaceAll("\\\\", "/");
-            define();
-        }
-
+    public static enum PluginType {
+        BUKKIT, BUNGEE
     }
 
-    private static void define() {
-        config = new Config();
-        temp = new Temp();
-        data = new Data();
-
-        temp.clear();
-
+    public static void initialize(@NotNull org.bukkit.plugin.java.JavaPlugin plugin) {
         try {
-            Lang.reload();
-        } catch (IOException | URISyntaxException e) {
+            type = PluginType.BUKKIT;
+            version = plugin.getDescription().getVersion();
+            corePath = plugin.getDataFolder().getAbsolutePath().replaceAll("\\\\", "/");
+
+            config = new Config();
+            data = new Data();
+            temp = new Temp();
+            temp.clear();
+
+            LangBukkit.reload();
+        } catch (Exception e) {
             e.printStackTrace();
+            ok = false;
         }
     }
 
+    public static void initialize(@NotNull net.md_5.bungee.api.plugin.Plugin plugin) {
+        try {
+            type = PluginType.BUNGEE;
+            version = plugin.getDescription().getVersion();
+            corePath = plugin.getDataFolder().getAbsolutePath().replaceAll("\\\\", "/");
 
-    public static class Bukkit {
-        public static org.bukkit.plugin.Plugin plugin;
-        public static org.bukkit.command.CommandMap commandMap;
+            config = new Config();
+            data = new Data();
+            temp = new Temp();
+            temp.clear();
 
-        public static void define () {
-            try {
-                Field f = org.bukkit.Bukkit.getServer().getClass().getDeclaredField("commandMap");
-                f.setAccessible(true);
-                commandMap = (org.bukkit.command.CommandMap) f.get(org.bukkit.Bukkit.getServer());
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
-                ok = false;
-            }
+            LangBungee.reload();
+        } catch (Exception e) {
+            e.printStackTrace();
+            ok = false;
         }
     }
 
-    public static class Bungee {
-        public static net.md_5.bungee.api.plugin.Plugin plugin;
-
-        public static void define () {
-
+    public static org.bukkit.command.CommandMap getCommandMap () {
+        try {
+            Field f = org.bukkit.Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            f.setAccessible(true);
+            return (org.bukkit.command.CommandMap) f.get(org.bukkit.Bukkit.getServer());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            ok = false;
+            return null;
         }
     }
-
 
 }
